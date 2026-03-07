4249c2de6b38857aed7e6370ac8936ee4c094843
erik.minasian <eminasian@webmd.net>
2026-03-07 13:38:36 +0400

    feat: pino logger added

--- comments-back-end/src/app.module.ts
+++ comments-back-end/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommentsModule } from './comments/comments.module';
@@ -9,7 +9,25 @@ import { LoggerModule } from 'nestjs-pino';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    LoggerModule.forRoot(),
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+
+        return {
+          pinoHttp: {
+            level: isProduction ? 'info' : 'debug',
+            transport: isProduction
+              ? undefined
+              : {
+                  target: 'pino-pretty',
+                  options: { singleLine: true, colorize: true },
+                },
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
     CommentsModule,
     UsersModule,
   ],
