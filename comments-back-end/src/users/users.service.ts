4490996fc88e90e2e9dfd72913eb71f8861511f1
erik.minasian <eminasian@webmd.net>
2026-03-15 15:43:46 +0400

    feat: integrate prisma in users service

--- comments-back-end/src/users/users.service.ts
+++ comments-back-end/src/users/users.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUsersDto } from './dto/create-users.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createUsersDto: CreateUsersDto) {
     // Implement user creation logic here
     console.log('Creating user with data:', createUsersDto);
