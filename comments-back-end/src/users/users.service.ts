1be3d62c2f932683541699bfe99aecab9279f352
erik.minasian <eminasian@webmd.net>
2026-03-18 11:02:47 +0400

    feat: error handling for repeated user creation

--- comments-back-end/src/users/users.service.ts
+++ comments-back-end/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { CreateUsersDto } from './dto/create-users.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -11,12 +11,20 @@ export class UsersService {
     const defaultUsername =
       `${createUsersDto.firstname.trim()}${createUsersDto.lastname.trim()}`.toLowerCase();
 
-    return await this.prismaService.user.create({
-      data: {
-        ...createUsersDto,
-        password: await bcrypt.hash(createUsersDto.password, 10),
-        username: defaultUsername,
-      },
-    });
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...createUsersDto,
+          password: await bcrypt.hash(createUsersDto.password, 10),
+          username: defaultUsername,
+        },
+      });
+    } catch (error) {
+      if ((error as { code: string })?.code === 'P2002') {
+        throw new UnprocessableEntityException('The user already exists!');
+      }
+
+      throw error;
+    }
   }
 }
