a9a20bd9bf299a4c7336c21effd0e613607bed4a
erik.minasian <eminasian@webmd.net>
2026-04-03 11:01:40 +0400

    fix: hide pages in header for logged out

--- comments-front-end/components/Header/Header.tsx
+++ comments-front-end/components/Header/Header.tsx
@@ -1,34 +1,31 @@
-'use client';
+"use client";
 
-import { useState, useContext } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import ShoppingBasketOutlined from '@mui/icons-material/ShoppingBasketOutlined';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import HeaderSettings from './HeaderSettings';
-import AuthContext from '@/contexts/authContext';
-import type { AuthContextType } from '@/contexts/authContext';
-import { isLoginView } from '@/utils/checks';
+import { useState, useContext } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import ShoppingBasketOutlined from "@mui/icons-material/ShoppingBasketOutlined";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import HeaderSettings from "./HeaderSettings";
+import AuthContext from "@/contexts/authContext";
+import type { AuthContextType } from "@/contexts/authContext";
+import { isLoginView } from "@/utils/checks";
 
-
-
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ["Products", "Pricing", "Blog"];
 
 function Header() {
+  const { isAuthenticated, user } = useContext<AuthContextType>(AuthContext);
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-  const { isAuthenticated, user } = useContext<AuthContextType>(AuthContext)
-  const searchParams = useSearchParams()
-  const router = useRouter()
-
-  const view = searchParams.get('view')
+  const view = searchParams.get("view");
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -42,62 +39,75 @@ function Header() {
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <ShoppingBasketOutlined sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <ShoppingBasketOutlined sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+        <Toolbar disableGutters sx={{ display: "flex", justifyContent: "space-between" }}>
+          {
+            isAuthenticated && (
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{ display: { xs: "block", md: "none" } }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography sx={{ textAlign: "center" }}>{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            )
+          }
+          <ShoppingBasketOutlined sx={{ mr: 1 }}/>
+          {
+            isAuthenticated && (
+                        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
                 {page}
               </Button>
             ))}
           </Box>
-          {
-            isAuthenticated ? 
-              <HeaderSettings user={user!} /> : 
-              <Button 
-                onClick={() => {router.push(`/auth?view=${isLoginView(view) ? 'signup' : 'login'}`)}}>
-                  {isLoginView(view) ? 'Signup' : 'Login'}
-              </Button>
+            )
           }
+
+          {isAuthenticated ? (
+            <HeaderSettings user={user!} />
+          ) : (
+            <Button
+              onClick={() => {
+                router.push(
+                  `/auth?view=${isLoginView(view) ? "signup" : "login"}`,
+                );
+              }}
+            >
+              {isLoginView(view) ? "Signup" : "Login"}
+            </Button>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
